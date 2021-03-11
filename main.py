5e79f505ab6f09fd2716408a991d23e297a20630
Corobo Bred <kingcorobobred@gmail.com>
2021-03-10 19:01:14 -0500

    Overlap images to render cat correctly.

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ class Cat(QtWidgets.QWidget):
     keys = []
     pressed_keys = []
     textures = {}
-    l_label = []
-    r_label = []
+    label = []
     layout = QtWidgets.QHBoxLayout()
 
     def __init__(self, keys):
@@ -17,16 +16,20 @@ class Cat(QtWidgets.QWidget):
         self.keys = keys
         self.pressed_keys = [False for i in range(len(self.keys))]
         self.load_textures()
-        self.l_label= QtWidgets.QLabel("left")
-        self.l_label.setPixmap(self.textures["l_00"])
-        self.r_label = QtWidgets.QLabel("right")
-        self.r_label.setPixmap(self.textures["r_00"])
-        self.layout.addWidget(self.l_label)
-        self.layout.addWidget(self.r_label)
+        self.label = QtWidgets.QLabel("left")
+        pix_map = self.textures["base"].copy()
+        painter = QtGui.QPainter(pix_map)
+        painter.drawPixmap(0, 0, self.textures["l_00"])
+        painter.drawPixmap(0, 0, self.textures["r_00"])
+        painter.end()
+        self.label.setPixmap(pix_map)
+        self.layout.addWidget(self.label)
         self.setLayout(self.layout)
 
     def load_textures(self):
         path = os.path.join(os.getcwd(), "images")
+        base = QtGui.QPixmap(os.path.join(path, "cat_base.png"))
+        self.textures["base"] = base
         l_00 = QtGui.QPixmap(os.path.join(path, "cat_l_00.png"))
         self.textures["l_00"] = l_00
 
@@ -65,8 +68,13 @@ class Cat(QtWidgets.QWidget):
 
         l_text = self.textures["l_" + str(int(self.pressed_keys[0])) + str(int(self.pressed_keys[1]))]
         r_text = self.textures["r_" + str(int(self.pressed_keys[2])) + str(int(self.pressed_keys[3]))]
-        self.l_label.setPixmap(l_text)
-        self.r_label.setPixmap(r_text)
+
+        pix_map = self.textures["base"].copy()
+        painter = QtGui.QPainter(pix_map)
+        painter.drawPixmap(0, 0, l_text)
+        painter.drawPixmap(0, 0, r_text)
+        painter.end()
+        self.label.setPixmap(pix_map)
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -80,7 +88,7 @@ class MainWindow(QtWidgets.QMainWindow):
         widget = QtWidgets.QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setStyleSheet("background-color: blue;")
         self.setWindowTitle("Bongo cat")
         self.show()
 
