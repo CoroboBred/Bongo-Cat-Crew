91b0de4dfda204e4117946c56f80c4b3fa1c8a51
Corobo Bred <kingcorobobred@gmail.com>
2021-03-12 01:28:54 -0500

    Add reverse 4 key cat. Refactor file reading.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from PyQt5 import QtWidgets, QtGui , QtCore# import PyQt5 widgets
+from PyQt5 import QtWidgets, QtCore  # import PyQt5 widgets
 import sys
-import os
 import keyboard
 import cat4k
 import cat1k
+import read
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -15,6 +15,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.cats = cats
         self.layout.setSpacing(0)
         self.layout.setContentsMargins(0, 0, 0, 0)
+
         for cat in self.cats:
             self.layout.addWidget(cat, 0, QtCore.Qt.AlignBottom)
         widget = QtWidgets.QWidget()
@@ -22,6 +23,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(widget)
         self.setStyleSheet("background-color: blue;")
         self.setWindowTitle("Bongo cat")
+
         self.show()
 
     def key_press(self, key, pressed):
@@ -40,78 +42,17 @@ def key_event(e):
     win.key_press(key, e.event_type == "down")
 
 
-def load_textures():
-    path = os.path.join(os.getcwd(), "images")
-    return {
-        "4k": load_4k_textures(path),
-        "1k": load_1k_textures(path),
-                }
-
-
-def load_1k_textures(path):
-    path = os.path.join(path, "1k_cat")
-
-    return {
-        "0":  QtGui.QPixmap(os.path.join(path, "1k_cat_0.png")),
-        "1":  QtGui.QPixmap(os.path.join(path, "1k_cat_1.png")),
-    }
-
-
-def load_4k_textures(path):
-    path = os.path.join(path, "4k_cat")
-    return {
-        "base": QtGui.QPixmap(os.path.join(path, "4k_cat_base.png")),
-        "l_00": QtGui.QPixmap(os.path.join(path, "4k_cat_l_00.png")),
-        "l_01": QtGui.QPixmap(os.path.join(path, "4k_cat_l_01.png")),
-        "l_10": QtGui.QPixmap(os.path.join(path, "4k_cat_l_10.png")),
-        "l_11": QtGui.QPixmap(os.path.join(path, "4k_cat_l_11.png")),
-        "r_00": QtGui.QPixmap(os.path.join(path, "4k_cat_r_00.png")),
-        "r_01": QtGui.QPixmap(os.path.join(path, "4k_cat_r_01.png")),
-        "r_10": QtGui.QPixmap(os.path.join(path, "4k_cat_r_10.png")),
-        "r_11": QtGui.QPixmap(os.path.join(path, "4k_cat_r_11.png")),
-    }
-
-
-def read_config():
-    cats_keys = {}
-
-    file = open("config.txt", "r")
-    file.readline()  # read intro line and empty line
-    file.readline()
-
-    file.readline()  # read 1-key title.
-    key = read_key(file.readline())
-    cats_keys["1k"] = key
-
-    file.readline()  # read 4-key title.
-    file.readline()
-    keys = []
-    for i in range(4):
-        keys.append(read_key(file.readline()))
-    cats_keys["4k"] = keys
-
-    file.close()
-
-    return cats_keys
-
-
-def read_key(line):
-    split = line.split("=")
-    if len(split) != 2:
-        raise str("incorrectly formatted line: ", line)
-    return split[1][0]
-
-
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    cats_keys = read_config()
+    cats_keys = read.read_config()
     global win
 
-    textures = load_textures()
-    cats = {
+    textures = read.load_textures()
+    cats = [
+        cat4k.Cat4k(cats_keys["4k_rev"], textures["4k_rev"]),
         cat1k.Cat1k(cats_keys["1k"], textures["1k"]),
         cat4k.Cat4k(cats_keys["4k"], textures["4k"]),
-    }
+    ]
     win = MainWindow(cats)
 
     keyboard.hook(key_event)
---
+++ read.py
@@ -0,0 +1,87 @@
+from PyQt5 import  QtGui  # import PyQt5 widgets
+import os
+
+def load_textures():
+    path = os.path.join(os.getcwd(), "images")
+    return {
+        "4k": load_4k_textures(path),
+        "1k": load_1k_textures(path),
+        "4k_rev": load_rev_4k_textures(path),
+    }
+
+
+def load_1k_textures(path):
+    path = os.path.join(path, "1k_cat")
+
+    return {
+        "0":  QtGui.QPixmap(os.path.join(path, "1k_cat_0.png")),
+        "1":  QtGui.QPixmap(os.path.join(path, "1k_cat_1.png")),
+    }
+
+
+def load_4k_textures(path):
+    path = os.path.join(path, "4k_cat")
+    return {
+        "base": QtGui.QPixmap(os.path.join(path, "4k_cat_base.png")),
+        "l_00": QtGui.QPixmap(os.path.join(path, "4k_cat_l_00.png")),
+        "l_01": QtGui.QPixmap(os.path.join(path, "4k_cat_l_01.png")),
+        "l_10": QtGui.QPixmap(os.path.join(path, "4k_cat_l_10.png")),
+        "l_11": QtGui.QPixmap(os.path.join(path, "4k_cat_l_11.png")),
+        "r_00": QtGui.QPixmap(os.path.join(path, "4k_cat_r_00.png")),
+        "r_01": QtGui.QPixmap(os.path.join(path, "4k_cat_r_01.png")),
+        "r_10": QtGui.QPixmap(os.path.join(path, "4k_cat_r_10.png")),
+        "r_11": QtGui.QPixmap(os.path.join(path, "4k_cat_r_11.png")),
+    }
+
+
+def load_rev_4k_textures(path):
+    path = os.path.join(path, "4k_cat")
+    return {
+        "base": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_base.png")),
+        "l_00": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_l_00.png")),
+        "l_01": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_l_01.png")),
+        "l_10": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_l_10.png")),
+        "l_11": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_l_11.png")),
+        "r_00": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_r_00.png")),
+        "r_01": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_r_01.png")),
+        "r_10": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_r_10.png")),
+        "r_11": QtGui.QPixmap(os.path.join(path, "4k_cat_rev_r_11.png")),
+    }
+
+
+def read_config():
+    cats_keys = {}
+
+    file = open("config.txt", "r")
+    file.readline()  # read intro line and empty line
+    file.readline()  # read blank line.
+
+    file.readline()  # read 1-key title.
+    key = read_key(file.readline())
+    cats_keys["1k"] = key
+
+    file.readline()  # read blank line.
+    file.readline()  # read 4-key title.
+    keys_4k = []
+    for i in range(4):
+        keys_4k.append(read_key(file.readline()))
+    cats_keys["4k"] = keys_4k
+
+    file.readline()  # read blank line.
+    file.readline()  # read 4-key reverse title.
+    keys_rev = []
+    for i in range(4):
+        keys_rev.append(read_key(file.readline()))
+    cats_keys["4k_rev"] = keys_rev
+
+    print(cats_keys)
+    file.close()
+
+    return cats_keys
+
+
+def read_key(line):
+    split = line.split("=")
+    if len(split) != 2:
+        raise str("incorrectly formatted line: ", line)
+    return split[1][0]
